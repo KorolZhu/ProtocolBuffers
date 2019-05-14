ec169d893eecbf1d35bdde185f219830a3b9be03
Zz <zhuzhi2013@gmail.com>
2019-05-14 20:50:09 +0800

    ProtocolBuffers for carthage

--- ProtocolBuffers/ProtocolBuffers.h
+++ ProtocolBuffers/ProtocolBuffers.h
@@ -16,4 +16,29 @@ FOUNDATION_EXPORT const unsigned char ProtocolBuffersVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ProtocolBuffers/PublicHeader.h>
 
-
+#import <ProtocolBuffers/Bootstrap.h>
+#import <ProtocolBuffers/AbstractMessage.h>
+#import <ProtocolBuffers/AbstractMessageBuilder.h>
+#import <ProtocolBuffers/CodedInputStream.h>
+#import <ProtocolBuffers/CodedOutputStream.h>
+#import <ProtocolBuffers/ConcreteExtensionField.h>
+#import <ProtocolBuffers/ExtendableMessage.h>
+#import <ProtocolBuffers/ExtendableMessageBuilder.h>
+#import <ProtocolBuffers/ExtensionField.h>
+#import <ProtocolBuffers/ExtensionRegistry.h>
+#import <ProtocolBuffers/Field.h>
+#import <ProtocolBuffers/GeneratedMessage.h>
+#import <ProtocolBuffers/GeneratedMessageBuilder.h>
+#import <ProtocolBuffers/Message.h>
+#import <ProtocolBuffers/MessageBuilder.h>
+#import <ProtocolBuffers/MutableExtensionRegistry.h>
+#import <ProtocolBuffers/MutableField.h>
+#import <ProtocolBuffers/PBArray.h>
+#import <ProtocolBuffers/RingBuffer.h>
+#import <ProtocolBuffers/UnknownFieldSet.h>
+#import <ProtocolBuffers/UnknownFieldSetBuilder.h>
+#import <ProtocolBuffers/Utilities.h>
+#import <ProtocolBuffers/WireFormat.h>
+#import <ProtocolBuffers/TextFormat.h>
+#import <ProtocolBuffers/Descriptor.pb.h>
+#import <ProtocolBuffers/ObjectivecDescriptor.pb.h>
